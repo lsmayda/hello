b869b792710ac1b8854e2e35058921de4d689127
lsmayda <l_smayda@yahoo.com>
2019-08-29 20:40:59 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jack Shanno and Liam Smayda
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jack Shanno and Liam Smayda\n");
+
+  return 0;
+
+}
